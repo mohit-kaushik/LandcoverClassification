c5d1e2ec65caf9540260a3df16c6c7ed56cb0bab
mohit <mohitkaushik2468@gmail.com>
2018-11-26 18:48:53 +0530

    Unet convo layer

---
+++ Unet.py
@@ -0,0 +1,14 @@
+from tensorflow.python.keras import losses
+from tensorflow.python.keras import models
+from tensorflow.python.keras import metrics
+from tensorflow.python.keras import optimizers
+
+def conv_block(input_x, n_filters):
+	# 3 *3 kernel size, no. of kernels = n_filters, padding=keep input size same
+	img_encoded = layers.Conv2D(n_filters, (3, 3), padding='same')(input_x)
+	img_encoded = layers.BatchNormalization()(img_encoded)
+	img_encoded = layers.Activation('relu')(img_encoded)
+	img_encoded = layers.Conv2D(num_filters, (3, 3), padding='same')(img_encoded)
+	img_encoded = layers.BatchNormalization()(img_encoded)
+	img_encoded = layers.Activation('relu')(img_encoded)
+	return img_encoded
