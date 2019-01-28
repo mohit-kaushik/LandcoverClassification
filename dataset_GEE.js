74f5a084429252da3332782461101f47d8fb6149
mohit <mohitkaushik2468@gmail.com>
2019-01-28 19:33:33 +0530

    nlcd added

--- dataset_GEE.js
+++ dataset_GEE.js
@@ -29,3 +29,47 @@ var image_landsat = imageCollection
 
 Map.addLayer(image_landsat,{min:0,max:0.3},"landsat")
 
+// 11-21 water ice
+// 22 -31 developed
+// 41 - 81 green
+// 90-95 wetlands
+
+var nlcd_water_ice = ee.Image('USGS/NLCD/NLCD2016')
+                .select('landcover')
+                .lte(21)
+
+var nlcd_dev1 = ee.Image('USGS/NLCD/NLCD2016')
+                .select('landcover')
+                .gte(22)
+
+var nlcd_dev2 = ee.Image('USGS/NLCD/NLCD2016')
+                .select('landcover')
+                .lte(31)
+
+var nlcd_dev = nlcd_dev1.and(nlcd_dev2)
+
+var nlcd_green1 = ee.Image('USGS/NLCD/NLCD2016')
+                .select('landcover')
+                .gte(41)
+
+var nlcd_green2 = ee.Image('USGS/NLCD/NLCD2016')
+                .select('landcover')
+                .lte(81)
+
+var nlcd_green = nlcd_green1.and(nlcd_green2)
+
+var nlcd_wetlands = ee.Image('USGS/NLCD/NLCD2016')
+                .select('landcover').gte(90)
+
+var nlcd_image = nlcd_water_ice
+              .addBands(nlcd_dev)
+              .addBands(nlcd_green)
+              .addBands(nlcd_wetlands)
+              .clip(region.geometry())
+              
+nlcd_image = nlcd_image.rename(['water','dev','vegi','wetland'])              
+var combined =  nlcd_image.addBands(image_landsat)
+
+Map.addLayer(combined,{},'nlcd lancover')
+
+
